9dc873f744a92e76f8e3054cf82442b4f3ae6425
Dharshan <dharshan.k@navikenz.com>
2026-05-16 14:28:12 +0530

    changes

--- K8s/helm-datadog.tf
+++ K8s/helm-datadog.tf
@@ -25,65 +25,81 @@ resource "helm_release" "datadog_otel" {
   ]
 
   values = [
-    <<-EOF
-    mode: deployment
-    image:
-      repository: "otel/opentelemetry-collector-contrib"
-
-    ports:
-      otlp:
-        enabled: true
-        containerPort: 4317
-        servicePort: 4317
-        protocol: TCP
-      otlp-http:
-        enabled: true
-        containerPort: 4318
-        servicePort: 4318
-        protocol: TCP
-
-    config:
-      receivers:
-        otlp:
-          protocols:
-            grpc:
-              endpoint: 0.0.0.0:4317
-            http:
-              endpoint: 0.0.0.0:4318
-
-      processors:
-        batch:
-          send_batch_size: 1000
-          timeout: 10s
-        resource:
-          attributes:
-            - key: cloud.platform
-              value: "azure_aks"
-              action: insert
-            - key: instrumentation.provider
-              value: "opentelemetry"
-              action: insert
-
-      exporters:
-        datadog:
-          api:
-            key: "${var.datadog_api_key}"
-          site: "${var.datadog_site}"
-
-      service:
-        pipelines:
-          traces:
-            receivers: [otlp]
-            processors: [resource, batch]
-            exporters: [datadog]
-          metrics:
-            receivers: [otlp]
-            processors: [resource, batch]
-            exporters: [datadog]
-          logs:
-            receivers: [otlp]
-            processors: [resource, batch]
-            exporters: [datadog]
-    EOF
+    jsonencode({
+      mode = "deployment"
+      image = {
+        repository = "otel/opentelemetry-collector-contrib"
+      }
+      extraEnvs = [
+        {
+          name  = "UPDATER_CACHE_BREAK"
+          value = var.datadog_api_key
+        }
+      ]
+      ports = {
+        otlp = {
+          enabled       = true
+          containerPort = 4317
+          servicePort   = 4317
+          protocol      = "TCP"
+        }
+        "otlp-http" = {
+          enabled       = true
+          containerPort = 4318
+          servicePort   = 4318
+          protocol      = "TCP"
+        }
+      }
+      config = {
+        receivers = {
+          otlp = {
+            protocols = {
+              grpc = { endpoint = "0.0.0.0:4317" }
+              http = { endpoint = "0.0.0.0:4318" }
+            }
+          }
+        }
+        processors = {
+          batch = {
+            send_batch_size = 1000
+            timeout         = "10s"
+          }
+          resource = {
+            attributes = [
+              { key = "cloud.platform", value = "azure_aks", action = "insert" },
+              { key = "instrumentation.provider", value = "opentelemetry", action = "insert" }
+            ]
+          }
+        }
+        exporters = {
+          datadog = {
+            #  FIXED: Natively nests site parameters within the authenticated API mapping engine blocks
+            api = {
+              key  = var.datadog_api_key
+              site = var.datadog_site
+            }
+          }
+        }
+        service = {
+          pipelines = {
+            traces = {
+              receivers  = ["otlp"]
+              processors = ["resource", "batch"]
+              exporters  = ["datadog"]
+            }
+            metrics = {
+              receivers  = ["otlp"]
+              processors = ["resource", "batch"]
+              exporters  = ["datadog"]
+            }
+            logs = {
+              receivers  = ["otlp"]
+              processors = ["resource", "batch"]
+              exporters  = ["datadog"]
+            }
+          }
+        }
+      }
+    })
   ]
 }
