ba4a14326089c38f31e37d51c148fb114a21501a
Dharshan <dharshan.k@navikenz.com>
2026-05-21 16:55:28 +0530

    try

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -6,3 +6,9 @@ export const AUTH_SERVICE_URL =
 
 export const AI_SERVICE_URL =
   window.appConfig?.VITE_AI_API_URL || "/api/ai";
+
+console.log("window.appConfig:", window.appConfig);
+
+console.log("PLANT_SERVICE_URL:", PLANT_SERVICE_URL);
+console.log("AUTH_SERVICE_URL:", AUTH_SERVICE_URL);
+console.log("AI_SERVICE_URL:", AI_SERVICE_URL);
